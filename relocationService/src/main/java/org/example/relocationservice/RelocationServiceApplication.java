5d28462bbd733b1519c24c44299298f4b752249a
GeorgFH <2210859013@fh-burgenland.at>
2025-03-21 13:18:07 +0100

    feat: implement initial project structure including failing health test for controller

---
+++ relocationService/src/main/java/org/example/relocationservice/RelocationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.relocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelocationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelocationServiceApplication.class, args);
+    }
+
+}
